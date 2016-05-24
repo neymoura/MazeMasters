22a2f82759f17bcc5cd49dc2e163d6fbc0538e34
Ney Moura <neymourac@gmail.com>
2016-05-24 08:25:07 -0300

    Implementação RMI

--- src/main/java/com/ioraptor/mazemasters/rmi/RMIMazeClient.java
+++ src/main/java/com/ioraptor/mazemasters/rmi/RMIMazeClient.java
@@ -2,8 +2,6 @@ package com.ioraptor.mazemasters.rmi;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
